8abda6233bb6206213414fdacb4dbdbe5b5a0666
Sunkara Krishna Teja <ktsunkara@gmail.com>
2021-04-02 09:57:54 +0530

    Added Global Exception Handling using @ControllerAdvice

--- src/main/java/com/skt/coveysQuad/controllers/TaskRestController.java
+++ src/main/java/com/skt/coveysQuad/controllers/TaskRestController.java
@@ -40,22 +40,4 @@ public class TaskRestController {
 		taskService.addTask(task);
 	}
 
-	@ExceptionHandler
-	public ResponseEntity<TaskErrorResponse> handleTaskNotFoundException(TaskNotFoundException e) {
-		TaskErrorResponse error = new TaskErrorResponse();
-		error.setStatus(HttpStatus.NOT_FOUND.value());
-		error.setMsg(e.getMessage());
-		error.setTimestamp(System.currentTimeMillis());
-		return new ResponseEntity<TaskErrorResponse>(error, HttpStatus.NOT_FOUND);
-	}
-
-	@ExceptionHandler
-	public ResponseEntity<TaskErrorResponse> handleTaskNotFoundException(Exception e) {
-		TaskErrorResponse err = new TaskErrorResponse();
-		err.setStatus(HttpStatus.BAD_REQUEST.value());
-		err.setMsg(e.getMessage());
-		err.setTimestamp(System.currentTimeMillis());
-		return new ResponseEntity<TaskErrorResponse>(err, HttpStatus.BAD_REQUEST);
-	}
-
 }
